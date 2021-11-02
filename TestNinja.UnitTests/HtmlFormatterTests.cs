58abb0ff85974da5d1f6a470b02ded17042aecac
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-02 16:30:19 -0600

    Testing strings

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+   [TestFixture]
+   public class HtmlFormatterTests
+   {
+
+      [Test]
+      public void FormatAsBold_WhenCalled_ShouldEncloseWithStringElement()
+      {
+         var formatter = new HtmlFormatter();
+
+         var res = formatter.FormatAsBold("abc");
+
+         //Specific
+         Assert.That(res, Is.EqualTo("<strong>abc</strong>"));
+
+         //More general
+         //Assert.That(res, Does.StartWith("<strong>"));
+         //Assert.That(res, Does.EndWith("</strong>"));
+         //Assert.That(res, Does.Contain("abc"));
+      }
+   }
+}
