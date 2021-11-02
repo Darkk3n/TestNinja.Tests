e5cb5106aa9ffca454a673deed107bf1cb256729
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-02 12:58:21 -0600

    Implemented Math tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+   [TestFixture]
+   public class MathTests
+   {
+      [Test]
+      public void Add_WhenCalled_ReturnsSumOfArguments()
+      {
+         var math = new Math();
+         var res = math.Add(1, 2);
+         Assert.That(res,Is.EqualTo(3));
+      }
+
+      [Test]
+      public void Max_FirstArgumentGreater_ReturnFirstArgument()
+      {
+         var math = new Math();
+         var res = math.Max(2, 1);
+         Assert.That(res, Is.EqualTo(2));
+      }
+
+      [Test]
+      public void Max_SecondArgumentGreater_ReturnSecondArgument()
+      {
+         var math = new Math();
+         var res = math.Max(1, 2);
+         Assert.That(res, Is.EqualTo(2));
+      }
+
+      [Test]
+      public void Max_EqualArguments_ReturnSameArgument()
+      {
+         var math = new Math();
+         var res = math.Max(2, 2);
+         Assert.That(res, Is.EqualTo(2));
+      }
+   }
+}
--- TestNinja/Fundamentals/Math.cs
+++ TestNinja/Fundamentals/Math.cs
@@ -2,23 +2,17 @@
 
 namespace TestNinja.Fundamentals
 {
-    public class Math
-    {
-        public int Add(int a, int b)
-        { 
-            return a + b;
-        }
-        
-        public int Max(int a, int b)
-        {
-            return (a > b) ? a : b;
-        }
+   public class Math
+   {
+      public int Add(int a, int b) => a + b;
 
-        public IEnumerable<int> GetOddNumbers(int limit)
-        {
-            for (var i = 0; i <= limit; i++)
-                if (i % 2 != 0)
-                    yield return i; 
-        }
-    }
+      public int Max(int a, int b) => (a > b) ? a : b;
+
+      public IEnumerable<int> GetOddNumbers(int limit)
+      {
+         for (var i = 0; i <= limit; i++)
+            if (i % 2 != 0)
+               yield return i;
+      }
+   }
 }
