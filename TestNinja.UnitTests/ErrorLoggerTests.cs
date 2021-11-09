01f40cc8ee4c8d3151a01001756fc95d6aff020f
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-09 16:33:28 -0600

    Void and expected exception tests

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+   [TestFixture]
+   public class ErrorLoggerTests
+   {
+      private ErrorLogger logger;
+
+      [SetUp]
+      public void Init()
+      {
+         logger = new ErrorLogger();
+      }
+
+      [Test]
+      public void Log_WhenCalled_SetTheLastErrorProperty()
+      {
+         logger.Log("Test");
+
+         Assert.That(logger.LastError, Is.EqualTo("Test"));
+      }
+
+      [Test]
+      [TestCase(null)]
+      [TestCase("")]
+      [TestCase(" ")]
+      public void Log_InvalidError_ThrowArgumentNullException(string error)
+      {
+         Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+      }
+   }
+}
