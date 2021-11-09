ca4a0a2182be793b89b53473c13ec303350e498d
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-09 16:25:24 -0600

    Return Type of Methods tests

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+   [TestFixture]
+   public class CustomerControllerTests
+   {
+      private CustomerController controller;
+
+      [SetUp]
+      public void SetUp()
+      {
+         controller = new CustomerController();
+      }
+
+      [Test]
+      public void GetCustomer_IdIsZero_ReturnNotFound()
+      {
+         var res = controller.GetCustomer(0);
+
+         // NotFound
+         Assert.That(res, Is.TypeOf<NotFound>());
+
+         //NotFound or one of its derivatives
+         //Assert.That(res, Is.InstanceOf<NotFound>());
+      }
+
+      [Test]
+      public void GetCustomer_IdIsOne_ReturnOk()
+      {
+         var res = controller.GetCustomer(1);
+
+         Assert.That(res, Is.TypeOf<Ok>());
+      }
+   }
+}
